6ff0b6029ce26861c38e65b5c7d1869c778a2d11
azzumjordhan <azzumjordhan14@gmail.com>
2018-04-27 16:32:34 +0700

    membuat fungsi login dan splash screen

---
+++ app/src/main/java/papb/com/presensicafe/User.java
@@ -0,0 +1,10 @@
+package papb.com.presensicafe;
+
+public class User {
+    private String id;
+    private String fullName;
+    private String email;
+    private String jamJaga;
+    private String role;
+    private String status;
+}
