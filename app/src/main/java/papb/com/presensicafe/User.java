a7fca9dd7149c2442e8e5374b31418092f0c3d4d
yanuwar ishak <yanuwarishak@gmail.com>
2018-04-27 17:51:24 +0700

    backend addnewpegawai selesai

--- app/src/main/java/papb/com/presensicafe/User.java
+++ app/src/main/java/papb/com/presensicafe/User.java
@@ -7,4 +7,52 @@ public class User {
     private long jamJaga;
     private String role;
     private String status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getJamJaga() {
+        return jamJaga;
+    }
+
+    public void setJamJaga(long jamJaga) {
+        this.jamJaga = jamJaga;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
